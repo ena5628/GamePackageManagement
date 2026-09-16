8a2530b0d4e9db288b812f2d233eab7b0df10e70
ena5628 <perfect.24ni.hakuto@gmail.com>
2026-09-16 11:08:30 +0900

    frontend/public/:*.js:fetchURLの形式をリバースプロキシに合合わせる

--- frontend/public/form.js
+++ frontend/public/form.js
@@ -226,7 +226,7 @@ const PlayTimeOverSolution = () =>{
 
 // fetchの非同期処理（insert）
 const FetchToFormInsert = async (formData) =>{
-    return await fetch(`${CONFIG.API_BASE_URL}/form/insert`, {
+    return await fetch(`/api/form/insert`, {
     method:'POST',
     body: formData
   });
@@ -252,7 +252,7 @@ const FetchToFormUpdate = async (formData) =>{
   if(oldImagePath){
     formData.append('OldImagePath',oldImagePath);
   }
-  return await fetch(`${CONFIG.API_BASE_URL}:3000/form/update`, {
+  return await fetch(`/api/form/update`, {
   method:'POST',
   body: formData
   });
@@ -368,7 +368,7 @@ const DeleteData = (gameData) =>{
 
       console.log('削除するID：' + DeleteId);  // 確認用
 
-      const response = await fetch(`${CONFIG.API_BASE_URL}/data/delete/${DeleteId}`);
+      const response = await fetch(`/api/data/delete/${DeleteId}`);
 
       const status = await response.text();
 
